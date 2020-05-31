d2ded582b6ade073669903031580552742476ed7
Bruce Tran <bruce.tran@gmail.com>
2020-05-31 19:45:24 -0400

    Add CityInfoContext. Add connectionStrings to appsettings . Create a test controller to create the empty tables.

---
+++ CityInfo.API/Controllers/TestDatabaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/testdatabase")]
+    public class TestDatabaseController : ControllerBase
+    {
+        private readonly CityInfoContext _ctx;
+
+        public TestDatabaseController(CityInfoContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        [HttpGet]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
