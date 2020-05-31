c1872a6b8891d717b3ac19e7dc8e565de36a93fd
Bruce Tran <bruce.tran@gmail.com>
2020-05-31 00:24:43 -0400

    Update startup.cs to use MVC and Add Cities Controller

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "Chicago"},
+                    new { id =2 , Name = "New York City"}
+                });
+        }
+    }
+}
